76dd7785a13aa920d6504c2e33701c4425ca11fa
GentoGaming <69847328+GentoGaming@users.noreply.github.com>
2020-08-24 10:38:29 +0200

    commit

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,63 +1,53 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Animator))]
-public class PlayerMovement : MonoBehaviour
+namespace Player_Scripts
 {
+    [RequireComponent(typeof(Animator))]
+    public class PlayerMovement : MonoBehaviour
+    {
 
-    private Vector2 _moveAxis;
+        private InputMaster _inputActions;
+        private static readonly int Strafe = Animator.StringToHash("Strafe");
+        
+        private Vector2 _moveAxis;
     
-    private Animator playerAnimator;
-    public float playerSpeed = 10f;
-    public float strafeSpeed = 11.0F;
-    
-    
-    private InputMaster _inputActions;
-    private void Awake()
-    {
-       playerAnimator = GetComponent<Animator>();
-       _inputActions = new InputMaster();
-    }
-
-    private void OnEnable()
-    {
-        _inputActions.Enable();
-    }
-
-
-    private void Start()
-    {
-        _inputActions.Player.Movement.performed +=  OnMovement;
-        _inputActions.Player.Movement.Enable();
-    }
-
-
-    private void OnMovement(InputAction.CallbackContext obj)
-    {
-        _moveAxis = obj.ReadValue<Vector2>();
-    }
+        private Animator _playerAnimator;
+        public float playerSpeed = 10f;
+        public float strafeSpeed = 11.0F;
     
     
-    private void OnDisable()
-    {
-        _inputActions.Disable();
-    }
 
-    private void Update()
-    {
-        playerAnimator.SetFloat("Strafe",_inputActions.Player.Movement.ReadValue<Vector2>().x);
-        transform.position += new Vector3(
-            _inputActions.Player.Movement.ReadValue<Vector2>().y * Time.deltaTime * -playerSpeed,
-            0,
-            _inputActions.Player.Movement.ReadValue<Vector2>().x * Time.deltaTime * strafeSpeed
-        );
-    }
+
+        private void Awake()
+        {
+            _playerAnimator = GetComponent<Animator>();
+            _inputActions = new InputMaster();
+        }
+
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+        }
+        
+    
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+        }
+
+        private void Update()
+        {
+            _playerAnimator.SetFloat(Strafe,_inputActions.Player.Movement.ReadValue<Vector2>().x);
+            transform.position += new Vector3(
+                _inputActions.Player.Movement.ReadValue<Vector2>().y * Time.deltaTime * -playerSpeed,
+                0,
+                _inputActions.Player.Movement.ReadValue<Vector2>().x * Time.deltaTime * strafeSpeed
+            );
+        }
 
     
 
 
+    }
 }
